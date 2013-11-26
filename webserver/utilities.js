b51038279abe0a8cf9a89ac27f089a09d28a066b
Marcus Sanatan <marcus@marcus-Aspire-5733.(none)>
2013-11-26 19:25:04 +0000

    Remove unused utilities file

--- webserver/utilities.js
+++
@@ -1,9 +0,0 @@
-exports.is_valid = function(ids) {
-    for (var i = 0; i < ids.length; i++) {
-        if (isNaN(ids[i])) {
-            return false;
-            console.log('An ID was incorrect');
-        }
-    }
-    return true;
-};
